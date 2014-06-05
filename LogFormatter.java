f1e46ef8244ea7742c0e3321fc5c2d2f7aded4f5
agent94ger <agent94@craftwolves.net>
2014-06-05 13:39:39 +0200

    Initial commit.

---
+++ LogFormatter.java
@@ -0,0 +1,12 @@
+package de.agent94ger.minecraft.LogFormatter;
+
+/**
+ * A simple cli created to compile Minecraft-serverlogs into HTML-files.
+ * 
+ * @author agent94ger
+ * @version Beta-1.0
+ */
+public class LogFormatter
+{
+    
+}
