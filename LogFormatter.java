ca59292b248077f1b0ea9e3ffa703c3367f63dc1
agent94ger <agent94@craftwolves.net>
2014-06-08 00:17:50 +0200

    Added possibility to enter filename of the log.
    * first command line argument is used for filename
    * if there's no argument, ask for the filename

--- LogFormatter.java
+++ LogFormatter.java
@@ -1,5 +1,9 @@
 package de.agent94ger.minecraft.LogFormatter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * A simple cli created to compile Minecraft-serverlogs into HTML-files.
  * 
@@ -8,5 +12,28 @@ package de.agent94ger.minecraft.LogFormatter;
  */
 public class LogFormatter
 {
+    private static String     _file;
     
+    public static void main(String[] args)
+    {
+        if (args.length > 0)
+        {
+            _file = args[0];
+        }
+        else
+        {
+            System.out.print("Enter file location:" + System.lineSeparator());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            try
+            {
+                _file = br.readLine();
+            }
+            catch (IOException ioe)
+            {
+                System.out.println("IO error trying to read the filename");
+                System.exit(1);
+            }
+        }
+        System.out.println(_file);
+    }
 }
