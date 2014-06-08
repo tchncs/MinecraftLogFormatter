7d02781283108828b28a3f593d8233c64e082ba8
agent94ger <agent94@craftwolves.net>
2014-06-08 14:29:09 +0200

    Added replacements for special chars

--- LogFormatter.java
+++ LogFormatter.java
@@ -1,8 +1,6 @@
 package de.agent94ger.minecraft.LogFormatter;
 
 import java.io.BufferedReader;
-// import java.io.FileReader;
-// import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -20,10 +18,7 @@ public class LogFormatter
 {
     private static String _file;
     
-    // private static FileReader _input;
-    // private static FileWriter _output;
-    
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
         if (args.length > 0)
         {
@@ -32,8 +27,7 @@ public class LogFormatter
         else
         {
             System.out.print("Enter file location:" + System.lineSeparator());
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    System.in));
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             try
             {
                 _file = br.readLine();
@@ -47,20 +41,51 @@ public class LogFormatter
         parseLog(_file);
     }
     
-    private static void parseLog(String filename) throws IOException
+    /**
+     * Reads the log line by line and calls the line parser
+     * 
+     * @param filename The complete path to the wanted log
+     */
+    private static void parseLog(String filename)
     {
         Path path = Paths.get(filename);
         try (Scanner scanner = new Scanner(path, StandardCharsets.UTF_8.name()))
         {
             while (scanner.hasNextLine())
             {
-                // process each line in some way
-                System.out.println(scanner.nextLine());
+                String line = scanner.nextLine();
+                line = parseLine(line);
+                System.out.println(line);
             }
         }
-        catch (Exception e) {
+        catch (Exception e)
+        {
             System.out.println("Error: File not found, please use absolute path");
             System.exit(1);
         }
+    }
+    
+    /**
+     * Parses the given line
+     * 
+     * @param line The line to parse
+     */
+    private static String parseLine(String line)
+    {
+        // Replace special characters
+        line = line.replace("&", "&amp;");
+        line = line.replace("<", "&lt;");
+        line = line.replace(">", "&gt;");
+        line = line.replace("ä", "&auml;");
+        line = line.replace("Ä", "&Auml;");
+        line = line.replace("ö", "&ouml;");
+        line = line.replace("Ö", "&Ouml;");
+        line = line.replace("ü", "&uuml;");
+        line = line.replace("Ü", "&Uuml;");
+        line = line.replace("ß", "&szlig;");
+        
+        // TODO: Add colors
+        
+        return line;
     }
 }
