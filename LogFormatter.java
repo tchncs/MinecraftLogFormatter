201502372c006129463f4105385ee7493599ce6e
agent94ger <agent94@craftwolves.net>
2014-06-08 15:32:58 +0200

    Added begin and end of HTML-file

--- LogFormatter.java
+++ LogFormatter.java
@@ -17,30 +17,73 @@ import java.util.Scanner;
 public class LogFormatter
 {
     private static String _file;
+    private static String _htmlBegin;
+    private static String _htmlEnd;
+    private static String _output;
     
     public static void main(String[] args)
     {
-        if (args.length > 0)
-        {
-            _file = args[0];
-        }
-        else
-        {
-            System.out.print("Enter file location:" + System.lineSeparator());
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            try
-            {
-                _file = br.readLine();
-            }
-            catch (IOException ioe)
-            {
-                System.out.println("IO error trying to read the filename");
-                System.exit(1);
-            }
-        }
+        _output = "";
+        
+        // Get file location via args or input
+        _file = initFile(args);
+        
+        // Initialize fields with HTML-code
+        initHTML();
+        
+        // Parse the log
         parseLog(_file);
+        
+        System.out.println(_output);
     }
     
+    /**
+     * Initializes file location
+     * 
+     * @param args Array of args
+     * @return File location
+     */
+    private static String initFile(String[] args)
+    {
+        String file = "/home/christian/test";
+        
+//        if (args.length > 0)
+//        {
+//            file = args[0];
+//        }
+//        else
+//        {
+//            System.out.print("Enter file location:" + System.lineSeparator());
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            try
+//            {
+//                file = br.readLine();
+//            }
+//            catch (IOException ioe)
+//            {
+//                System.out.println("IO error trying to read the filename");
+//                System.exit(1);
+//            }
+//        }
+        
+        return file;
+    }
+    
+    /**
+     * 
+     */
+    private static void initHTML()
+    {
+        _htmlBegin  = "<html>" + System.lineSeparator();
+        _htmlBegin += "<head>" + System.lineSeparator();
+        _htmlBegin += "<title>Colored Minecraft log</title>" + System.lineSeparator();
+        _htmlBegin += "</head>" + System.lineSeparator();
+        _htmlBegin += "<body bgcolor=#000000>" + System.lineSeparator();
+
+        _htmlEnd  = "</body>" + System.lineSeparator();
+        _htmlEnd += "</html>" + System.lineSeparator();
+    }
+
     /**
      * Reads the log line by line and calls the line parser
      * 
@@ -51,12 +94,14 @@ public class LogFormatter
         Path path = Paths.get(filename);
         try (Scanner scanner = new Scanner(path, StandardCharsets.UTF_8.name()))
         {
+            _output = _htmlBegin;
             while (scanner.hasNextLine())
             {
                 String line = scanner.nextLine();
                 line = parseLine(line);
-                System.out.println(line);
+                _output += line + System.lineSeparator();
             }
+            _output += _htmlEnd;
         }
         catch (Exception e)
         {
@@ -84,18 +129,18 @@ public class LogFormatter
         line = line.replace("Ü", "&Uuml;");
         line = line.replace("ß", "&szlig;");
         
-        // Add colors
+        // Add colors and new lines
         if (line.contains("WARN"))
         {
-            line = "<span style=\"color:#FF0000\">" + line + "</span>";
+            line = "<span style=\"color:#FF0000\">" + line + "</span><br />";
         }
         else if (line.contains("INFO"))
         {
-            line = "<span style=\"color:#FFFFFF\">" + line + "</span>";
+            line = "<span style=\"color:#FFFFFF\">" + line + "</span><br />";
         }
         else
         {
-            line = "<span style=\"color:#0000FF\">" + line + "</span>";
+            line = "<span style=\"color:#FF0000\">" + line + "</span><br />";
         }
         
         return line;
